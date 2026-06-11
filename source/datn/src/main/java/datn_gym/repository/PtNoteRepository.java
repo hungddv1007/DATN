3134ddaf5f06981bcbcbd435d54cb16ef42ab99e
Kudoha <you@example.com>
2026-06-11 23:26:10 +0700

    xoa thu vien thua

--- source/datn/src/main/java/datn_gym/repository/PtNoteRepository.java
+++ source/datn/src/main/java/datn_gym/repository/PtNoteRepository.java
@@ -4,14 +4,12 @@ import datn_gym.entity.PtNote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface PtNoteRepository extends JpaRepository<PtNote, Integer> {
     List<PtNote> findByPt_IdAndMember_IdOrderByCreatedAtDesc(Integer ptId, Integer memberId);
- 
+
     List<PtNote> findByPt_IdOrderByCreatedAtDesc(Integer ptId);
- 
+
     // FIX IDOR + Truy vấn dư thừa:
     // Check ownership tại DB trong 1 câu SQL thay vì lấy lên rồi check bằng Java
     Optional<PtNote> findByIdAndPt_Id(Integer id, Integer ptId);
